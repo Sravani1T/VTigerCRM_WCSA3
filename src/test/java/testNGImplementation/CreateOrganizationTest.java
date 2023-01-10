8613d5f7ba078cff760496da8241a73bd94ade58
Sravani1T <122075001+Sravani1T@users.noreply.github.com>
2023-01-10 15:06:01 +0530

    s

--- src/test/java/testNGImplementation/CreateOrganizationTest.java
+++ src/test/java/testNGImplementation/CreateOrganizationTest.java
@@ -32,7 +32,7 @@ public class CreateOrganizationTest extends BaseClass
 		softAssert.assertTrue(organization.getPageHeader().contains("Organization"));
 		
 		organization.clickPlusButton();
-		softAssert.assertTrue(createOrganization.getPageHeader().contains("Creating  Organization"));
+		softAssert.assertTrue(createOrganization.getPageHeader().contains("Creating New  Organization"));
 		
 		
 		Map<String,String> map = excel.getDataBasedOnKey("TestData", "Create Organization");
